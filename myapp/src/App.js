f89383264b3396a7314425471e818676b43aae4f
Apsara2001 <139642058+Apsara2001@users.noreply.github.com>
2024-11-19 14:31:57 +0000

    StudentTable layout out and styles are added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {students} from './data/StudentDb';
+import StudentTable from './components/StudentTable';
 function App() {
   return (
     <div className="App">
@@ -10,7 +11,7 @@ function App() {
       <tbody>
         <tr>
           <td>
-            
+            <studentTable students={students}/>
           </td>
         </tr>
       </tbody>
